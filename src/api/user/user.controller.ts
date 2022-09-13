e0f986e01333780a48bb4d4c870674a627e6d893
Ksanbal <dev.ksanbal@gmail.com>
2022-09-13 17:26:17 +0900

    Feat: User 회원가입 기능 개발

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,10 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from '../auth/auth.service';
 import { LoginDto } from '../auth/dtos/login.dto';
+import { JoinUserDto } from './dtos/joinUser.dto';
+import { UserService } from './user.service';
 
+@ApiTags('회원관리')
 @Controller('api/user')
 export class UserController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post('join')
+  join(@Body() joinUserDto: JoinUserDto) {
+    return this.userService.join(joinUserDto);
+  }
 
   @Post('login')
   login(@Body() loginDto: LoginDto) {
