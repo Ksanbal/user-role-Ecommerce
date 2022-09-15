83284076f324679aa528bada177f6bb17ea88672
Ksanbal <dev.ksanbal@gmail.com>
2022-09-15 14:42:40 +0900

    Feat: 사용자 Entity에 address 추가

--- src/api/user/dtos/user.dto.ts
+++ src/api/user/dtos/user.dto.ts
@@ -1,11 +1,17 @@
 import { PickType } from '@nestjs/swagger';
 import { UserEntity } from '../entities/user.entity';
 
-export class UserDto extends PickType(UserEntity, ['id', 'email', 'createAt']) {
-  constructor(userEntity: UserEntity) {
+export class UserDto extends PickType(UserEntity, [
+  'id',
+  'email',
+  'address',
+  'createAt',
+]) {
+  constructor(user: UserEntity) {
     super();
-    this.id = userEntity.id;
-    this.email = userEntity.email;
-    this.createAt = userEntity.createAt;
+    this.id = user.id;
+    this.email = user.email;
+    this.address = user.address;
+    this.createAt = user.createAt;
   }
 }
